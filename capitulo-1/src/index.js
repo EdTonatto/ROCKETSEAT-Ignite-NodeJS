8ad50bbe328d247b53d4b5c30388070d601013e3
Eduardo Tonatto <edtonatto@gmail.com>
2022-03-02 22:02:31 -0300

    :sparkles: Novo endpoint para retorno de statement

--- capitulo-1/src/index.js
+++ capitulo-1/src/index.js
@@ -3,7 +3,7 @@ const {v4: uuidv4} = require("uuid")
 
 const app = express()
 app.use(express.json())
-
+ 
 const customers = []
 
 app.post("/account", (request, response) => {
@@ -24,6 +24,17 @@ app.post("/account", (request, response) => {
     })
 
     return response.status(201).send();
+})
+
+app.get("/statement/:cpf", (request, response) => {
+    const {cpf} = request.params
+
+    const customer = customers.find((customer) => customer.cpf === cpf)
+
+    if(!customer)
+        return response.status(400).json({error: "Customer not found!"})
+
+    return response.json(customer.statement)
 })
 
 app.listen(3333)
