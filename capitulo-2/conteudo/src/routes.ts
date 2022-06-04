a84e7674d9da4036302dfaa1c689bb3fec932c12
Eduardo Tonatto <edtonatto@gmail.com>
2022-06-04 16:05:05 -0300

    :sparkles: Novo endpoint com parâmetros tipados

---
+++ capitulo-2/conteudo/src/routes.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response){
+    CreateCourseService.execute({
+        duration: 10,
+        name: "NodeJS", 
+        educator: "Educator Name"
+    });
+
+    return response.send();
+}
