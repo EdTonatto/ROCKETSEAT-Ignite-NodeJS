ba21b7d48ce95170be7cd9d0b3635147de0973cf
Eduardo Tonatto <edtonatto@gmail.com>
2022-06-17 22:56:06 -0300

    :tada: Commit inicial do capitulo 4 com aplicação desenvolvida ate fim do capitulo 3

---
+++ capitulo-4/conteudo/rentx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,44 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    id,
+    name,
+    email,
+    driver_license,
+    password,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      id,
+      name,
+      email,
+      driver_license,
+      avatar,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOneBy({ email });
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOneBy({ id });
+    return user;
+  }
+}
+
+export { UsersRepository };
